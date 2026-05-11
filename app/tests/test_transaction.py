4ae213ff9bca10bd7f9c3ade1989f084599dd147
matheusrmds20 <matheusrmds14@gmail.com>
2026-05-11 20:07:15 -0300

    More tests

--- app/tests/test_transaction.py
+++ app/tests/test_transaction.py
@@ -83,4 +83,71 @@ def test_sem_categoria(db_session, default_category):
     assert str(excinfo.value) == "ID de Categoria nao se coicidem"
 
 def test_limite_maior_0(db_session, default_category):
-    pass
+    service_t = TransactionService(db_session)
+
+    with pytest.raises(ValueError) as excinfo:
+        a = service_t.list_transaction(
+                user_id =  1,
+                limit = 0,
+                page = 1,
+        )
+
+    print(excinfo)
+    
+    assert str(excinfo.value) == "Valor limite tem que ser maior do que 0"
+
+def test_paginacao_maior_0(db_session, default_category):
+    service_t = TransactionService(db_session)
+
+    with pytest.raises(ValueError) as excinfo:
+        a = service_t.list_transaction(
+                user_id =  1,
+                limit = 1,
+                page = 0,
+        )
+
+    assert str(excinfo.value) == "Valor de paginas tem ser maior do que 0"
+
+
+def test_transacao_nao_encontrada(db_session, default_category):
+    service_t = TransactionService(db_session)
+
+    with pytest.raises(ValueError) as excinfo:
+        a = service_t.list_transaction(
+            user_id = 1,
+            limit = 1,
+            page = 1
+            )
+
+    assert str(excinfo.value) == "Transacao nao econtrada"
+
+def test_limite_alto(db_session, default_category):
+    service_t = TransactionService(db_session)
+
+    data = TransactionsDB(
+        title = "teste",
+        value = 100,
+        type = "income",
+        user_id = 1,
+        category_id  = default_category.id
+    )
+    
+    transacao = service_t.create_transaction(1, data)
+
+
+    with pytest.raises(ValueError) as excinfo:
+        a = service_t.list_transaction(
+            user_id = 1,
+            limit = 101,
+            page = 1
+        )
+    
+    assert str(excinfo.value) == "Limite muito alto"
+
+def test_listagem_id(db_session, default_category):
+    service_t = TransactionService(db_session)
+
+    with pytest.raises(ValueError) as excinfo:
+        service_t.list_transaction_id(1, 1)
+
+    assert str(excinfo.value) == "Transacao nao encontrada"
